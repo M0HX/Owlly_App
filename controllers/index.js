6f7c3ca6dae3ec443c2788ab35f7e28dfb2cf370
m0hx777 <m0hx@protonmail.com>
2023-12-10 14:04:57 +0300

    uwu

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,6 @@
 exports.index_get = (req, res) => {
     //res.send("Welcome to Blog App");
     //res.render('home/index');  
-    res.render('home/index', {message: "Welcome to Blog App"})  
+    res.render('home/index', {message: "Welcome to Owlly App"})  
 }
 
